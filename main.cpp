ffc9bf54049906519c2445e05b84b0d72444f062
Petrenko Hryhorii <hryhorii.petrenko@student.howest.be>
2025-08-29 21:28:38 +0200

    Adjustments of the FireModes logic

--- main.cpp
+++ main.cpp
@@ -11,26 +11,29 @@ enum class ECaliber {
     TwelveGauge
 };
 
+enum class EFireModes {
+    Safe = 0,
+    Semi = 1,
+    Burst = 2,
+    FAuto = 3,
+};
+
 class Firearm {
 private:
     std::string name;
     ECaliber caliber;
     int magCapacity;
     int maxCapacity;
-    std::vector<std::string> fireModes;
+    std::vector<EFireModes> fireModes;
+    EFireModes currentFireMode;
     
 public:
-    Firearm(std::string _name,
-            ECaliber _caliber,
-            int _magCapacity,
-            int _maxCapacity,
-            std::vector<std::string> _fireModes
-    ) {
+    Firearm(std::string _name, ECaliber _caliber, int _magCapacity, int _maxCapacity) {
         name = _name;
         caliber = _caliber;
         magCapacity = _magCapacity;
         maxCapacity = _maxCapacity;
-        fireModes = _fireModes;
+        fireModes = { EFireModes::Safe, EFireModes::Semi };
     }
 
     std::string getName() { return name; }
@@ -44,7 +47,7 @@ public:
 
     void Reload() {}
     void Fire() {}
-    void changeFiringMode() {}
+    void nextFiringMode() {}
 };
 
 int main() {
