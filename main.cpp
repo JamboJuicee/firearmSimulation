dbc2feae0526ebf596fcf03ed9c2b7dac37f128b
Petrenko Hryhorii <hryhorii.petrenko@student.howest.be>
2025-08-29 20:54:05 +0200

    Changed enum calss for ECaliber, Added some more variables to the class

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 enum class ECaliber {
-    NineMM,
+    NineEighteen,
+    NineNineteen,
     FiveFortyFive,
     FiveFiveSix,
     SevenSixTwo,
@@ -14,20 +16,35 @@ private:
     std::string name;
     ECaliber caliber;
     int magCapacity;
+    int maxCapacity;
+    std::vector<std::string> fireModes;
+    
 public:
-    Firearm(std::string _name, ECaliber _caliber, int _magCapacity) {
+    Firearm(std::string _name,
+            ECaliber _caliber,
+            int _magCapacity,
+            int _maxCapacity,
+            std::vector<std::string> _fireModes
+    ) {
         name = _name;
         caliber = _caliber;
         magCapacity = _magCapacity;
+        maxCapacity = _maxCapacity;
+        fireModes = _fireModes;
     }
 
     std::string getName() { return name; }
     ECaliber    getCaliber() { return caliber; }
     int         getMagCapacity() { return magCapacity; }
+    int         getMaxAmmoInChamber() const { return maxCapacity - magCapacity; }
 
     void setName(std::string _name) { name = _name; }
     void setCaliber(ECaliber _caliber) { caliber = _caliber; }
     void setMagCapacity(int _magCapacity) { magCapacity = _magCapacity; }
+
+    void Reload() {}
+    void Fire() {}
+    void changeFiringMode() {}
 };
 
 int main() {
